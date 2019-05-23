86a4ec8cfcce3ba0dfeb203736926630fa6b0243
Jiuli Gao <gaojiuli@gmail.com>
2019-05-23 15:48:00 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,14 @@
 sudo apt-get update
 sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-sudo apt install zsh
-sudo apt install neovim
-sudo apt install pipenv
 sudo apt install git
 sudo apt install autojump
+sudo apt install zsh
+chsh -s $(which zsh)
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 curl https://pyenv.run | bash
 curl -L git.io/antigen > ~/.antigen.zsh
 
-chsh -s $(which zsh)
 echo "source ~/.antigen.zsh
 antigen use oh-my-zsh
 antigen bundle git
